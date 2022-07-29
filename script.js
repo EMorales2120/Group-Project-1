7d7cfcf8d04753e7bb0f0d2d3a6b749d8139001e
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-07-28 19:00:27 -0700

    update1

--- script.js
+++
@@ -1,9 +0,0 @@
-let activeTab = 1, allData;
-const allTabsBody = document.querySelectorAll('.tab-body-single');
-const alltabsHead = document.querySelectorAll('.tab-head-single');
-const searchForm = document.querySelector('.app-header-search');
-let searchList = document.getElementById('search-list');
-const init = () => { 
-    showActiveTabBody();
-
-}
