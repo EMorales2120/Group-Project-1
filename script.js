89a2cb97e9b6a372b42fc1dc1a87226acc0952dd
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-08-01 18:54:34 -0700

    update

---
+++ script.js
@@ -0,0 +1,60 @@
+
+const allTabsBody = document.querySelectorAll('.tab-body-single');
+const alltabsHead = document.querySelectorAll('.tab-head-single');
+const searchForm = document.querySelector('.app-header-search');
+let searchList = document.getElementById('search-list');
+
+let activeTab = 1, allData;
+
+const init = () => { 
+    showActiveTabBody();
+    showActiveTabHead();
+} 
+
+const showActiveTabHead = () => alltabsHead[activeTab - 1].classList.add('active-tab'); 
+  
+const showActiveTabBody = () => {
+    hideAllTabBody()
+        allTabsBody[activeTab - 1].classList.add('show-tab');
+    
+}
+
+const hideAllTabBody = () => allTabsBody.forEach(singleTabBody => singleTabBody.classList.remove('show-tab'))
+const hideAllTabHead = () => alltabsHead.forEach(singleTabHead => singleTabHead.classList.remove('active-tab'))
+
+// event listenor
+
+window.addEventListener('DOMContentLoaded', () => init()); 
+// button event listeners
+alltabsHead.forEach(singleTabHead => {
+    singleTabHead.addEventListener('click', () => {
+        hideAllTabBody(); 
+        activeTab = singleTabHead.dadaset.id;
+        showActiveTabHead();
+
+    })
+})
+
+const getInputValue = (event) => {
+    event.preventDefault();
+    let searchText = searchForm.search.value;
+    fetchAllSuperHero(searchText);
+}
+
+// search for submissiom
+searchForm.addEventListener('submit', getInputValue); 
+
+//api key: 5561133997271918
+
+const fetchAllSuperHero = async(searchText) => {
+    let url = `https://www.superheroapi.com/api.php/5561133997271918/search/${searchText}`;
+    try{
+        const respone = await fetch(url);
+        allData = await respone.json();
+        if(allData.respone === 'success'){
+            showSearchList(allData.results);
+        }
+    } catch(error){
+        console.log(error);
+    }
+}
